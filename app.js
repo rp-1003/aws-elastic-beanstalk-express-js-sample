8b130fee4762d0bc3f6c047eb86ccd8b26803226
roopagap <roopagap@yahoo.com>
2022-02-26 23:05:52 +0000

    Full pipeline test

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('aws-ELASTICBEAN-REPO-NVIRGINA-RP'));
+app.get('/', (req, res) => res.send('aws-ELASTICBEAN-REPO-NVIRGINA-RP-review-stage added-after the successful buid completion version2 of the code'));
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
